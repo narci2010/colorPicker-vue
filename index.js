32fc416aa71720bcb43e6049149895fe090fd4f9
邱致武 <chuchur@qq.com>
2017-08-12 13:51:08 +0800

    修复多种模式下的显示影藏

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-/*!
- * 一个vue 的颜色组件 ，欢迎使用
- * create by chuchur /chuchur@qq.com
- * date:2017-01-18 10:02:16
- * version：1.0.2
- * author: chuchur
- */
-import colorPicker from './src/picker'
-export default colorPicker
+import colorPicker from './src/colorPicker'
+export default  colorPicker
